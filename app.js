c42880a3987ecfc2270fb7b39be8f35ae92d486d
Otto Mättas <ottomattas@gmail.com>
2023-08-16 19:06:47 +0300

    Draw indicator on canvas for previous selection

--- app.js
+++ app.js
@@ -12,20 +12,30 @@ canvas.addEventListener('click', function(event) {
     const x = event.offsetX;
     const y = event.offsetY;
     const pixel = ctx.getImageData(x, y, 1, 1).data;
-    
-    console.log(`Clicked at: ${x}, ${y}`);  // Log the click coordinates
-    console.log(`Pixel data: ${pixel}`);    // Log the pixel data
 
     // Check if the pixel is white or transparent
     if (pixel[0] === 255 && pixel[1] === 255 && pixel[2] === 255 || pixel[3] === 0) {
-        console.log("Clicked on white or transparent pixel");  // Log this for clarity
         return;  // Ignore white or transparent pixels
     }
 
     const rgba = `rgba(${pixel[0]}, ${pixel[1]}, ${pixel[2]}, ${pixel[3] / 255})`;
-    
     document.getElementById('chosenColor').innerText = rgba;
-    
+
+    // Draw the indicator on the canvas
+    drawIndicator(x, y);
+
     // Send the color choice to the backend
     // Update the frontend based on the response
 });
+
+function drawIndicator(x, y) {
+    // First, redraw the color wheel to clear any previous indicators
+    ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+
+    // Now, draw the indicator (a small circle) at the clicked coordinates
+    const indicatorRadius = 10;  // Adjust as needed
+    ctx.beginPath();
+    ctx.arc(x, y, indicatorRadius, 0, 2 * Math.PI, false);
+    ctx.fillStyle = 'black';  // Color of the indicator
+    ctx.fill();
+}
